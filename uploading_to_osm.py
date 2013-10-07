89fdde90af3ce375fbdfaf07b85c8ce0347a6be9
amritkarma <amrit.im@gmail.com>
2013-10-07 19:58:47 +0545

    can read rows from database

--- uploading_to_osm.py
+++ uploading_to_osm.py
@@ -3,13 +3,13 @@ import sys
 
 con = None
 
-con = lite.connect('../building_id/table_ko_name.db')
+con = lite.connect('../building_id/dev_db.db')
 with con:
     print con
     
     cur = con.cursor()    
-    cur.execute('SELECT SQLITE_VERSION()')
-    
-    data = cur.fetchone()
-    
-    print "SQLite version: %s" % data
+    cur.execute('SELECT * from psuedonumber')
+    rows = cur.fetchall()
+
+    for row in rows:
+    	print row
