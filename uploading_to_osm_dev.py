52b37677b2f2ed9d54ac8d27e5d6086d2426233b
amritkarma <amrit.im@gmail.com>
2013-11-24 14:53:36 +0545

    testing the data

--- uploading_to_osm_dev.py
+++ uploading_to_osm_dev.py
@@ -13,32 +13,23 @@ print 'python version=',sys.version
 ## declaring global variables
 building_count = 0
 surveyors_error = {}
-new_building_not_found = 0
+new_building_not_found = []
 new_building = 0
 overpass_api   = "http://overpass-api.de/api/interpreter?data="
 con = None
 con = lite.connect('../building_id/dev_db.db')
 main_db = lite.connect('../building_id/main_pseudonumber_db - Copy.db')
-# with con:
-#     print con
-    
-#     main_cur = con.cursor()    
-    # main_cur.execute('SELECT * from psuedonumber')
-    # rows = main_cur.fetchall()
-
-    # for row in rows:
-    # 	print row
 
 ## Getting data from internet
 print("Getting data from internet")
-# formhub_request = urllib2.Request('https://formhub.org/nirabpudasaini/forms/fullexposure_form_new/api')
-# try:
-#     formhub_response = urllib2.urlopen(formhub_request)
-#     print "successful"
-# except urllib2.URLError:
-#     print('We failed to reach a server.')
-#     print('Reason: ', e.reason)
-formhub_response = open('data_from_formhub_oct_29.json')
+formhub_request = urllib2.Request('https://formhub.org/nirabpudasaini/forms/fullexposure_form_new_oct_27/api')
+try:
+    formhub_response = urllib2.urlopen(formhub_request)
+    print "successful"
+except urllib2.URLError as e:
+    print('We failed to reach a server.')
+    print('Reason: ', e)
+# formhub_response = open('data_from_formhub_oct_7_part_overpass.json')
 
 print "loading data"
 json_data = json.loads(formhub_response.read())
@@ -125,7 +116,7 @@ for building in json_data:
                 osm_id   = element['id']
                 print "osm_id",osm_id
             else:
-                new_building_not_found += 1
+                new_building_not_found.append(overpass_tag)
                 print overpass_tag,"Not found"
                 surveyor_id = building['surveyor_id']
                 surveyors_error.setdefault(surveyor_id,0)
@@ -145,125 +136,149 @@ for building in json_data:
 
 
     #after id is determined
-    # if(osm_id):
-    #     print "retrieving data from main osm for way",osm_id
-    #     old_data = convert(MainApi.WayFull(osm_id)
+    if(osm_id):
+        print "retrieving data from main osm for way",osm_id
+        try:
+            old_data = convert(MainApi.WayFull(osm_id)) # in dev
+            old_building_data_dev = old_data[-1]['data']
+            lat = 0.0
+            lon = 0.0
+            count=0
+            for element in old_data:
+                if(element['type']=='node'):
+                    lat += element['data']['lat']
+                    lon += element['data']['lon']
+                    count += 1
+                old_building_data_dev['lat'] = lat/count
+                old_building_data_dev['lon'] = lon/count
+            old_building_data_main = convert(MainApi.WayGet(osm_id)) # in main
+            # pprint(old_building_data_dev)
+            # pprint(old_building_data_main)
+            # print old_building_data_dev==old_building_data_main
 
-    #     if(not old_data['data']['tag']['building:structure']):
+        except urllib2.URLError:
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+
+        if(old_building_data_dev['tag'].get('building:structure',None)):
+            # remove ['data'] in master branch
             
-    #         #preserving old data
-    #         new_data = {'tag':old_data['data']['tag']}
+            #preserving old data
+            new_data = {'tag':old_building_data_dev['tag']}
+
+            # only in dev version
             
-    #         #building ownership
-    #         if(building["building_own"] == "building_own_rent"):
-    #             new_data['tag']['building:ownership'] = "rent"
-    #         else:
-    #             new_data['tag']['building:ownership'] = "self"
+            #building ownership
+            if(building["building_own"] == "building_own_rent"):
+                new_data['tag']['building:ownership'] = "rent"
+            else:
+                new_data['tag']['building:ownership'] = "self"
 
-    #         # building structural system
-    #         if(building["building_struct"] == "building_struct_erc"):
-    #             new_data['tag']["building:structure"] = "engineered_reinforced_concrete"
-    #         elif(building["building_struct"] == "building_struct_nrc"):
-    #             new_data['tag']["building:structure"] = "non_engineered_reinforced_concrete"
-    #         elif(building["building_struct"] == "building_struct_sc"):
-    #             new_data['tag']["building:structure"] = "load_bearing_stone_wall_in_cement_mortar"
-    #         elif(building["building_struct"] == "building_struct_bls"):
-    #             new_data['tag']["building:structure"] = "load_bearing_brick_wall_in_lime_surkhi_mortar"
-    #         elif(building["building_struct"] == "building_struct_bc"):
-    #             new_data['tag']["building:structure"] = "load_bearing_brick_wall_in_cement_mortar"
-    #         elif(building["building_struct"] == "building_struct_bm"):
-    #             new_data['tag']["building:structure"] = "load_bearing_brick_wall_in_mud_mortar"
-    #         elif(building["building_struct"] == "building_struct_dsm"):
-    #             new_data['tag']["building:structure"] = "load_bearing_dry_stone_wall"
-    #         elif(building["building_struct"] == "building_struct_sm"):
-    #             new_data['tag']["building:structure"] = "load_bearing_stone_wall_in_mud_mortar"
-    #         elif(building["building_struct"] == "building_struct_ad"):
-    #             new_data['tag']["building:structure"] = "adobe"
+            # building structural system
+            if(building["building_struct"] == "building_struct_erc"):
+                new_data['tag']["building:structure"] = "engineered_reinforced_concrete"
+            elif(building["building_struct"] == "building_struct_nrc"):
+                new_data['tag']["building:structure"] = "non_engineered_reinforced_concrete"
+            elif(building["building_struct"] == "building_struct_sc"):
+                new_data['tag']["building:structure"] = "load_bearing_stone_wall_in_cement_mortar"
+            elif(building["building_struct"] == "building_struct_bls"):
+                new_data['tag']["building:structure"] = "load_bearing_brick_wall_in_lime_surkhi_mortar"
+            elif(building["building_struct"] == "building_struct_bc"):
+                new_data['tag']["building:structure"] = "load_bearing_brick_wall_in_cement_mortar"
+            elif(building["building_struct"] == "building_struct_bm"):
+                new_data['tag']["building:structure"] = "load_bearing_brick_wall_in_mud_mortar"
+            elif(building["building_struct"] == "building_struct_dsm"):
+                new_data['tag']["building:structure"] = "load_bearing_dry_stone_wall"
+            elif(building["building_struct"] == "building_struct_sm"):
+                new_data['tag']["building:structure"] = "load_bearing_stone_wall_in_mud_mortar"
+            elif(building["building_struct"] == "building_struct_ad"):
+                new_data['tag']["building:structure"] = "adobe"
 
-    #         # no of stories
-    #         if(building["building_stories"]):
-    #             new_data['tag']['building:levels'] = building["building_stories"]
+            # no of stories
+            if(building["building_stories"]):
+                new_data['tag']['building:levels'] = building["building_stories"]
 
-    #         # building use
-    #         if(building["building_use"] == "building_use_res"):
-    #             new_data['tag']['building:use'] = 'residential'
-    #         elif(building["building_use"] == "building_use_edu"):
-    #             new_data['tag']['building:use'] = 'education'
-    #         elif(building["building_use"] == "building_use_hel"):
-    #             new_data['tag']['building:use'] = 'health'
-    #         elif(building["building_use"] == "building_use_com"):
-    #             new_data['tag']['building:use'] = 'commercial'
-    #         elif(building["building_use"] == "building_use_res"):
-    #             new_data['tag']['building:use'] = 'residential'
+            # building use
+            if(building["building_use"] == "building_use_res"):
+                new_data['tag']['building:use'] = 'residential'
+            elif(building["building_use"] == "building_use_edu"):
+                new_data['tag']['building:use'] = 'education'
+            elif(building["building_use"] == "building_use_hel"):
+                new_data['tag']['building:use'] = 'health'
+            elif(building["building_use"] == "building_use_com"):
+                new_data['tag']['building:use'] = 'commercial'
+            elif(building["building_use"] == "building_use_res"):
+                new_data['tag']['building:use'] = 'residential'
 
-    #         # floor material type
-    #         if(building["building_flt"] == "building_flt_rig"):
-    #             new_data['tag']['floor:material:type'] = 'rigid'
-    #         elif(building["building_flt"] == "building_flt_fle"):
-    #             new_data['tag']['floor:material:type'] = 'flexible'
+            # floor material type
+            if(building["building_flt"] == "building_flt_rig"):
+                new_data['tag']['floor:material:type'] = 'rigid'
+            elif(building["building_flt"] == "building_flt_fle"):
+                new_data['tag']['floor:material:type'] = 'flexible'
 
-    #         # roof material type
-    #         if(building["building_rft"] == "building_rft_rig"):
-    #             new_data['tag']['roof:material:type'] = 'rigid'
-    #         elif(building["building_rft"] == "building_rft_fle"):
-    #             new_data['tag']['roof:material:type'] = 'flexible'
+            # roof material type
+            if(building["building_rft"] == "building_rft_rig"):
+                new_data['tag']['roof:material:type'] = 'rigid'
+            elif(building["building_rft"] == "building_rft_fle"):
+                new_data['tag']['roof:material:type'] = 'flexible'
 
-    #         # building attachment
-    #         if(building["building_attach"] == "building_attach_true"):
-    #             new_data['tag']['building:adjacency'] = "yes"
-    #         elif(building["building_attach"] == "building_attach_false"):
-    #             new_data['tag']['building:adjacency'] = "no"
+            # building attachment
+            if(building["building_attach"] == "building_attach_true"):
+                new_data['tag']['building:adjacency'] = "yes"
+            elif(building["building_attach"] == "building_attach_false"):
+                new_data['tag']['building:adjacency'] = "no"
 
-    #         # shape in plan
-    #         if(building["building_shape_plan"] == "building_shape_plan_r"):
-    #             new_data['tag']['shape:plan'] = "regular"
-    #         elif(building["building_shape_plan"] == "building_shape_plan_r"):
-    #             new_data['tag']['shape:plan'] = "irregular"
+            # shape in plan
+            if(building["building_shape_plan"] == "building_shape_plan_r"):
+                new_data['tag']['shape:plan'] = "regular"
+            elif(building["building_shape_plan"] == "building_shape_plan_r"):
+                new_data['tag']['shape:plan'] = "irregular"
 
-    #         # shape in elevation
-    #         if(building["building_shape_elev"] == "building_shape_elev_r"):
-    #             new_data['tag']['shape:elevation'] = "regular"
-    #         elif(building["building_shape_elev"] == "building_shape_elev_ir"):
-    #             new_data['tag']['shape:elevation'] = "irregular"
-    #         elif(building["building_shape_elev"] == "building_shape_elev_nt"):
-    #             new_data['tag']['shape:elevation'] = "narrow_tall"
-    #         elif(building["building_shape_elev"] == "building_shape_elev_ntir"):
-    #             new_data['tag']['shape:elevation'] = "narrow_tall_irregular"
+            # shape in elevation
+            if(building["building_shape_elev"] == "building_shape_elev_r"):
+                new_data['tag']['shape:elevation'] = "regular"
+            elif(building["building_shape_elev"] == "building_shape_elev_ir"):
+                new_data['tag']['shape:elevation'] = "irregular"
+            elif(building["building_shape_elev"] == "building_shape_elev_nt"):
+                new_data['tag']['shape:elevation'] = "narrow_tall"
+            elif(building["building_shape_elev"] == "building_shape_elev_ntir"):
+                new_data['tag']['shape:elevation'] = "narrow_tall_irregular"
 
-    #         # visible physical condition
-    #         if(building["building:condition"] == "building_condition_poor"):
-    #             new_data['tag']['physical_condition'] = "poor"
-    #         elif(building["building:condition"] == "building_condition_avg"):
-    #             new_data['tag']['physical_condition'] = "average"
-    #         elif(building["building:condition"] == "building_condition_good"):
-    #             new_data['tag']['physical_condition'] = "good"
+            # visible physical condition
+            if(building["building:condition"] == "building_condition_poor"):
+                new_data['tag']['physical_condition'] = "poor"
+            elif(building["building:condition"] == "building_condition_avg"):
+                new_data['tag']['physical_condition'] = "average"
+            elif(building["building:condition"] == "building_condition_good"):
+                new_data['tag']['physical_condition'] = "good"
 
-    #         # building overhang
-    #         if(building["building_overhang"]=="building_overhang_false"):
-    #             new_data['tag']['building:overhang'] = 'no'
-    #         elif(building["building_overhang"]=="building_overhang_true"):
-    #             new_data['tag']['building:overhang'] = 'yes'
+            # building overhang
+            if(building["building_overhang"]=="building_overhang_false"):
+                new_data['tag']['building:overhang'] = 'no'
+            elif(building["building_overhang"]=="building_overhang_true"):
+                new_data['tag']['building:overhang'] = 'yes'
 
-    #         # building soft storey
-    #         if(building["building_softstor"]=="building_softstor_false"):
-    #             new_data['tag']['building:soft_storey'] = 'no'
-    #         elif(building["building_softstor"]=="building_softstor_true"):
-    #             new_data['tag']['building:soft_storey'] = 'yes'
+            # building soft storey
+            if(building["building_softstor"]=="building_softstor_false"):
+                new_data['tag']['building:soft_storey'] = 'no'
+            elif(building["building_softstor"]=="building_softstor_true"):
+                new_data['tag']['building:soft_storey'] = 'yes'
 
-    #         # building gable wall
-    #         if(building["building_gable"]=="building_gable_false"):
-    #             new_data['tag']['building:gable_wall'] = 'no'
-    #         elif(building["building_gable"]=="building_gable_true"):
-    #             new_data['tag']['building:gable_wall'] = 'yes'
+            # building gable wall
+            if(building["building_gable"]=="building_gable_false"):
+                new_data['tag']['building:gable_wall'] = 'no'
+            elif(building["building_gable"]=="building_gable_true"):
+                new_data['tag']['building:gable_wall'] = 'yes'
 
-    #     else:
-    #         print "Data exists for way",osmid
+        else:
+            print "Data exists for way",osm_id
 
-    #     print DevApi.NodeCreate(new_data)
-    # #
+        # print DevApi.NodeCreate(new_data)
+    #
 # DevApi.ChangesetClose()
 
 print "No of buildings",building_count
 print "surveyors_error",surveyors_error
 print "new_building",new_building
 print "new_building_not_found",new_building_not_found
+print "new_building_not_found no",len(new_building_not_found)
